cb90bd044d6e329c4c1b260cdda021faef2ce880
Michael Misciagna <mmisciagna@google.com>
2023-01-31 12:54:40 -0800

    Cont adding content to About page

--- src/routes/about/about.tsx
+++ src/routes/about/about.tsx
@@ -4,23 +4,28 @@ import {RESUME_CONTENT} from './resume';
 const Intro = () => {
   return (
     <>
-      <section className="mm-section">
-        <h1>[ mee-shah-nyah ]</h1>
+      <section className="mm-section mm-about__intro">
+        <div className=" mm-grid">
+          <div className="mm-grid__col-r">
+            <h1 className="mm-page-title">Howdy, I am Michael Misciagna.</h1>
+            <h2 className="mm-page-subtext">pronounced [mee-shah-nyah]</h2>
+          </div>
+        </div>
       </section>
-      <section className="mm-section mm-section--full-bleed mm-about__intro">
+      <section className="mm-section mm-section--full-bleed mm-about__tldr">
         <div className="mm-grid mm-section__inner">
           <div className="mm-grid__col-l">
-            <h2>TL;DR</h2>
+            <img src="/static/imgs/profile-pic.webp" alt="Profile picture" />
           </div>
           <div className="mm-grid__col-r">
-            <h3>Sr. UX Engineer at Google,<br />with over 12+ years of experience.</h3>
+            <h2>Sr. UX Engineer at Google, with over 12+ years of experience.</h2>
             <p>I have developed and designed the front-ends of massively trafficked websites.</p>
             <p>The current state of web production requires developers to have a design eye and understanding of user experience. No longer does it suffice to have designers and developers work in silos. Instead, they need to collaborate and understand the other’s role and capabilities. Having worked as both a front-end developer and designer, I have a proven ability to bridge the gap between these cross-disciplinary teams.</p>
-            <h3>Technical prowess</h3>
+            <h2>Technical prowess</h2>
             <p>I am not beholden to any one front-end platform but am not against them either. I believe in using the right tool for the right project. I am proficient in JavaScript and TypeScript, and that affords me the ability to quickly become an expert in the platform du jour. The advantage of being an experienced developer is having the flexibility and wherewithal to find the best tool for the project.</p>
-            <h3>Design experience</h3>
+            <h2>Design experience</h2>
             <p>As someone with both UX and UI design experience, as well as engineering expertise, I am able to create designs that are responsive across all devices and screen sizes. Additionally, I am able to quickly create and test prototypes within the browser as needed.</p>
-            <h3>I care about performance</h3>
+            <h2>I care about performance</h2>
             <p>I want to see our projects produce the desired results we set out to achieve, and not pat ourselves on the back just for launching something. I want to see them “land”.  When given a project, I will complete it from start to launch and see it through its lifetime.</p>
           </div>
         </div>
