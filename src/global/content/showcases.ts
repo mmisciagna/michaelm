06dd1577e52dbe0d0f295f24b9d037141b29bbeb
Michael Misciagna <mmisciagna@google.com>
2023-02-04 21:45:26 -0800

    Finish cropping showcase thumbs

--- src/global/content/showcases.ts
+++ src/global/content/showcases.ts
@@ -86,7 +86,7 @@ export const SHOWCASES = [
     title: 'YouTube Impact Lab',
     client: 'YouTube',
     role: 'Frontend Engineer',
-    img: 'youtube-impact-lab.jpeg',
+    img: 'youtube-impact-lab.webp',
     description: [
       `Targeting non-profits, YouTube Impact Lab's mission is to harness YouTube's scale, technology and talent to ignite and sustain movements that drive measurable social change.`,
     ],
@@ -98,7 +98,7 @@ export const SHOWCASES = [
     title: 'YouTube Creators',
     client: 'YouTube',
     role: 'Frontend Engineer',
-    img: 'youtube-creators.jpeg',
+    img: 'youtube-creators.webp',
     description: [
       `The YouTube Creators site is a resource hub for all YouTube creators, whether they are just getting starting or already have 1,000,000+ subscribers. There they'll find video tutorials, contest sign ups, the latest news from the Creators social feeds and general information on how to optimize their YouTube Channel.`,
     ],
@@ -109,7 +109,7 @@ export const SHOWCASES = [
     title: 'YouTube Creators, design concept',
     client: 'YouTube',
     role: 'UX Designer',
-    img: 'youtube-creators-design.jpeg',
+    img: 'youtube-creators-design.webp',
     description: [
       `I removed the carousel in favor of an image of a popular YouTube Creator. Mulitples studies have shown that carousels are ineffective for displaying content because most ignore anything after the first frame.`,
       `The copy states exactly what this site is — a resource hub for YouTube creators — with clear calls to action.`,
@@ -120,7 +120,7 @@ export const SHOWCASES = [
     title: 'Google ACUX Doc Center',
     client: 'Google',
     role: 'Frontend Engineer',
-    img: 'acux-doc-center.jpg',
+    img: 'acux-doc-center.webp',
     description: [
       `Design and prototype an experience that would make it easy for agencies with one to hundreds of clients to find any document in one centralized, filterable location.`,
       `Google has an international customer base. This means that transactions with them can involve many different currencies, not just US dollars. This project needed to be able to scale and accommodate for such cases by being smart enough to recognize that and have different actions for different currencies. In this example, there are 2 Brazillian documents that have options to open the document in a new window.`,
@@ -132,7 +132,7 @@ export const SHOWCASES = [
     title: 'YouTube Creators for Change',
     client: 'YouTube',
     role: 'Frontend Engineer',
-    img: 'youtube-creators-for-change.jpeg',
+    img: 'youtube-creators-for-change.webp',
     description: [
       `A global initiative that supports YouTube creators who are tackling social issues and promoting awareness, tolerance and empathy on their YouTube channels.`,
       `Showcase YouTube creators who have made significant advancements in addressing awareness on their YouTube channels. This site makes it known that YouTube Creators for Change is truely a global initiative by displaying the creators' country of origin and all of the various global chapters.`,
