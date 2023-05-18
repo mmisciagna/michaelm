479aef5074274b007df5c5ff03a9279117e8cd76
mmisciagna <mmisciagna86@gmail.com>
2023-05-17 19:09:40 -0700

    Add Swiper.js to Shop section

--- src/for-fun/bakery/components/Card.tsx
+++ src/for-fun/bakery/components/Card.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 
-function Card({details}: {details: {
+export interface CardShape {
   type: string,
   imgSrc?: string,
   title: string,
   price: string,
   description: string,
-}}) {
+}
+
+const typeColorMap: Record<string, string[]> = {
+  'pizza': ['hsl(120 35% 25%)', '#fff'],
+  'bread': ['#fff'],
+  'small-bites': ['hsl(0 45% 40%)', '#fff'],
+}
+
+function Card({details}: {details: CardShape}) {
+  const colorKey = details.type.toLowerCase().split(' ').join('-');
+  const typeBG = typeColorMap[colorKey][0];
+  const typeColor = typeColorMap[colorKey][1];
+
   return (
     <div className="card">
       <div className="card__details">
-        <div className="card__img fpo"></div>
+        <div className="card__img fpo flex-center">
+          FPO
+        </div>
         <div className="card__type">
-          {details.type}
+          <span
+            style={{
+              background: typeBG,
+              color: typeColor || 'inherit',
+            }}
+          >
+            {details.type}
+          </span>
         </div>
         <div>
           <h4 className="card__title">
--- src/for-fun/bakery/components/Shop.tsx
+++ src/for-fun/bakery/components/Shop.tsx
@@ -1,5 +1,61 @@
 import React from 'react';
-import Card from './Card';
+import { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Card, {CardShape} from './Card';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
+const cards: CardShape[] = [
+  {
+    type: 'Pizza',
+    title: 'Pizza Margharita',
+    price: '$18.00',
+    description: `Pizza Margherita is a classic Italian pizza made with a thin crust, tomato sauce, fresh mozzarella cheese, basil, and extra-virgin olive oil. It is a simple yet delicious pizza that highlights the quality of the ingredients used.`,
+  },
+  {
+    type: 'Pizza',
+    title: 'Salsiccia Pizza',
+    price: '$22.00',
+    description: `Salsiccia Pizza is a delicious pizza that is sure to satisfy any pizza lover's cravings. This pizza features a thin, crispy crust that is topped with tangy tomato sauce, fresh mozzarella cheese, and savory Italian sausage.`,
+  },
+  {
+    type: 'Bread',
+    title: 'Foccacia',
+    price: '$24.00',
+    description: `We make our focaccia in-house, using only the finest ingredients and traditional techniques. Our focaccia is baked to perfection, with a golden-brown crust and a fragrant aroma that will tantalize your taste buds. It is the perfect addition to any meal.`
+  },
+  {
+    type: 'Small Bites',
+    title: 'Taralli',
+    price: '$14.00',
+    description: `Taralli are small, crispy, and savory Italian biscuits with a unique and irresistible texture. They are made from simple ingredients such as flour, water, olive oil, white wine, and salt. The result is a crispy and crunchy texture with a subtle savory flavor that makes them a perfect snack or accompaniment to a variety of dishes.`
+  },
+  {
+    type: 'Pizza',
+    title: 'Pizza Margharita',
+    price: '$18.00',
+    description: `Pizza Margherita is a classic Italian pizza made with a thin crust, tomato sauce, fresh mozzarella cheese, basil, and extra-virgin olive oil. It is a simple yet delicious pizza that highlights the quality of the ingredients used.`,
+  },
+  {
+    type: 'Pizza',
+    title: 'Salsiccia Pizza',
+    price: '$22.00',
+    description: `Salsiccia Pizza is a delicious pizza that is sure to satisfy any pizza lover's cravings. This pizza features a thin, crispy crust that is topped with tangy tomato sauce, fresh mozzarella cheese, and savory Italian sausage.`,
+  },
+  {
+    type: 'Bread',
+    title: 'Foccacia',
+    price: '$24.00',
+    description: `We make our focaccia in-house, using only the finest ingredients and traditional techniques. Our focaccia is baked to perfection, with a golden-brown crust and a fragrant aroma that will tantalize your taste buds. It is the perfect addition to any meal.`
+  },
+  {
+    type: 'Small Bites',
+    title: 'Taralli',
+    price: '$14.00',
+    description: `Taralli are small, crispy, and savory Italian biscuits with a unique and irresistible texture. They are made from simple ingredients such as flour, water, olive oil, white wine, and salt. The result is a crispy and crunchy texture with a subtle savory flavor that makes them a perfect snack or accompaniment to a variety of dishes.`
+  },
+];
 
 function Shop() {
   return (
@@ -8,40 +64,20 @@ function Shop() {
         <h2 className="eyebrow">
           Order Online
         </h2>
-        <div className="grid grid--4cols" style={{ columnGap: '1em', rowGap: '3em' }}>
-          <div>
-            <Card details={{
-              type: 'Pizza',
-              title: 'Pizza Margharita',
-              price: '$18.00',
-              description: `Pizza Margherita is a classic Italian pizza made with a thin crust, tomato sauce, fresh mozzarella cheese, basil, and extra-virgin olive oil. It is a simple yet delicious pizza that highlights the quality of the ingredients used.`,
-            }} />
-          </div>
-          <div>
-            <Card details={{
-              type: 'Pizza',
-              title: 'Salsiccia Pizza',
-              price: '$22.00',
-              description: `Salsiccia Pizza is a delicious pizza that is sure to satisfy any pizza lover's cravings. This pizza features a thin, crispy crust that is topped with tangy tomato sauce, fresh mozzarella cheese, and savory Italian sausage.`,
-            }} />
-          </div>
-          <div>
-            <Card details={{
-              type: 'Bread',
-              title: 'Foccacia',
-              price: '$24.00',
-              description: `We make our focaccia in-house, using only the finest ingredients and traditional techniques. Our focaccia is baked to perfection, with a golden-brown crust and a fragrant aroma that will tantalize your taste buds. It is the perfect addition to any meal.`
-            }} />
-          </div>
-          <div>
-            <Card details={{
-              type: 'Small Bites',
-              title: 'Taralli',
-              price: '$14.00',
-              description: `Taralli are small, crispy, and savory Italian biscuits with a unique and irresistible texture. They are made from simple ingredients such as flour, water, olive oil, white wine, and salt. The result is a crispy and crunchy texture with a subtle savory flavor that makes them a perfect snack or accompaniment to a variety of dishes.`
-            }} />
-          </div>
-        </div>
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={24}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          breakpoints={{
+            600: {slidesPerView: 2},
+            900: {slidesPerView: 3},
+            1200: {slidesPerView: 4},
+          }}
+        >
+          {cards.map((card: CardShape) => <SwiperSlide><Card details={card} /></SwiperSlide>)}
+        </Swiper>
       </div>
     </section>
   );
