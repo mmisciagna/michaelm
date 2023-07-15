a7281ab4de9c00c0f9cad7d3019aa7fdeb3b368c
mmisciagna <mmisciagna86@gmail.com>
2023-07-14 20:53:58 -0700

    Revert next config to default

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  distDir: 'dist',
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
